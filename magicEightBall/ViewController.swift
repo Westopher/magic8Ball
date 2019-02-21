b8c4f76e8a4fcad95d1fb482c253490269e1f763
West Kraemer <kraemerwest@gmail.com>
2019-02-21 15:38:00 -0500

    add update ball function to viewdidload and button

--- magicEightBall/ViewController.swift
+++ magicEightBall/ViewController.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallIndex = 0
+    
     @IBOutlet weak var eightBallImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateBall()
+        
     }
 
     @IBAction func ballButtonPressed(_ sender: UIButton) {
+        updateBall()
     }
     
+    
+    func updateBall() {
+        randomBallIndex = Int.random(in: 0 ... 4)
+        print(randomBallIndex)
+        eightBallImage.image = UIImage(named: ballArray[randomBallIndex])
+    }
 }
 
